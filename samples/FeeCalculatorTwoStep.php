1de3d27bf642167f86e43c2bdc929b4b250409f7
Paul Hulett <paulhulett@gmail.com>
2017-04-17 14:34:29 -0700

    v1.5 Deals Fee Calc Example
    FeeCalculatorTwoStep.php updated to use v1.5 Deals and Deal Transactions endpoints.

--- samples/FeeCalculatorTwoStep.php
+++ samples/FeeCalculatorTwoStep.php
@@ -13,32 +13,32 @@ $retryDelayBase = 3;
 
 // create a deal with the required fields, request fees after getting a deal with decoded vin info back
 $api = new AVRSAPI();
-$api->setURL('/api/v1/deals/');
+$api->setURL('/api/v1.5/deals/');
 $api->setMethod('POST');
-$api->addPayload('owners', [[
-                                'zip' => 95492,
-                            ]]);
-$api->addPayload('vehicles', [[
-                                  'cost'                => 45000,
-                                  'first-operated-date' => '2017-11-11',
-                                  'first-sold-date'     => '2017-11-11',
-                                  'vin'                 => '3B7HC13YXYG105749',
-                                  'type-license-code'   => 11,
-
-                              ]]);
-$api->addPayload('transaction-type', 3);
-$api->addPayload('gateway-type', 'CALC-CA');
+$dealData = [
+    'owners' => [['zip' => 95492]],
+    'vehicles' => [[
+                        'cost'                => 45000,
+                        'first-operated-date' => '2017-11-11',
+                        'first-sold-date'     => '2017-11-11',
+                        'vin'                 => 'JH4KA8250MC004002',
+                        'type-license-code'   => 11,
+                    ]],
+    'transaction-type' => 3,
+    'gateway-type' => 'CALC-CA',
+];
+$api->addPayload('deals', [$dealData]);
 $api->send();
 $response = json_decode($api->getResult(), true);
 Writer::writeRequestResponse($api);
 $api = new AVRSAPI();
-$api->setURL('/api/v1/deals/');
-$api->setMethod('PUT');
-$api->addPayload('status', 'QF');
-$api->addPayload('id', $response['deals'][0]['id']);
+$api->setURL('/api/v1.5/deals/transactions/');
+$api->setMethod('POST');
+$api->addPayload('deal-status', 'FR');
+$api->addPayload('deal-id', $response['deals'][0]['id']);
 $api->send();
 $response = json_decode($api->getResult(), true);
-while ($retryAttempts++ < $retryMax && $response['deals'][0]['error-code'] == 'CADMV/Q023') {
+while ($retryAttempts++ < $retryMax && isset($response['error']) && $response['error']['code'] == 'CADMV/Q023') {
     error_log('DMV Retry Code Encountered');
     sleep($retryDelayBase * pow(2, $retryAttempts));
     $api->send();
