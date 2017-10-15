de818c7717c65c2187c4caa77cd79678b355ff83
Paul Hulett <paulhulett@gmail.com>
2017-10-15 14:01:38 -0700

    Refactor renewRegistration example
    PSR-4 refactor missed this file somehow

--- examples/renewRegistration.php
+++ examples/renewRegistration.php
@@ -1,73 +1,47 @@
 <?php
-namespace samples;
-if (!(class_exists(__NAMESPACE__ . '\Loader'))) {
-	require_once(realpath(__DIR__ . '/loader.php'));
-}
-use api\AVRSAPI as AVRSAPI;
-use api\TestRecords as TestRecords;
+namespace PaulJulio\AvrsApi\Examples;
 
-$bitmask = (TestRecords::BIT_MOTORCYCLE | TestRecords::BIT_RENEWAL_DUE);
-// exponential backoff settings
-$retryAttempts  = 0;
-$retryMax       = 3;
-$retryDelayBase = 3;
+use PaulJulio\AvrsApi\TestRecords;
 
-// do we have an inventory reservation already?
-// check from newest reservation to oldest
+class renewRegistration extends AbstractExample {
 
-$haveReservation = false;
-$reservations = array_reverse(TestRecords::getRecords());
-foreach ($reservations as $reservation) {
-	if (($reservation['conditions'] & $bitmask) == $bitmask) {
-		$haveReservation = true;
-		break;
-	}
-}
-if (!$haveReservation) {
-	$reservation = TestRecords::reserveRecord($bitmask);
-}
+    public function run() {
 
-// create a deal with the required fields and save
-$api = new AVRSAPI();
-$api->setURL('/api/v1/deals/');
-$api->setMethod('POST');
-$api->addPayload('vehicles', array(array(
-	'vin'       => $reservation['vin']   ,
-	'plate'     => $reservation['plate'] ,
-	'insurance' => 'Y'                   , // for testing environment only, certify that the vehicle is insured
-)));
-$api->addPayload('transaction-type', 6);
-$api->send();
-$response = json_decode($api->getResult(), true);
-Writer::writeRequestResponse($api);
-sleep(1);
-// create a deal transaction for our deal, providing the desired end state (FR, FP or C)
-$api = new AVRSAPI();
-$api->setURL('/api/v1.5/deals/transactions/');
-$api->setMethod('POST');
-$api->addPayload('deal-id', $response['deals'][0]['id']);
-$api->addPayload('status', 'FR'); // getting fees, which should be checked for sanity
-while ($retryAttempts++ < $retryMax && $response['error']['code'] == 'CADMV/Q023') {
-	error_log('DMV Retry Code Encountered');
-	sleep($retryDelayBase * pow(2, $retryAttempts));
-	$api->send();
-	$response = json_decode($api->getResult(), true);
-    Writer::writeRequestResponse($api);
+        $bitmask = (TestRecords::BIT_MOTORCYCLE | TestRecords::BIT_RENEWAL_DUE);
+        $reservation = $this->getTestRecord($bitmask);
+
+        // create a deal with the required fields and save
+        $this->api = new AVRSAPI();
+        $this->api->setURL('/api/v1.5/deals/');
+        $this->api->setMethod('POST');
+        $this->api->addPayload('vehicles', [[
+           'vin'       => $reservation['vin']   ,
+           'plate'     => $reservation['plate'] ,
+           'insurance' => 'Y'                   , // for testing environment only, certify that the vehicle is insured
+       ]]);
+        $this->api->addPayload('transaction-type', 6);
+        $this->send();
+        $response = json_decode($this->api->getResult(), true);
+        $this->logApi();
+        sleep(1);
+        // create a deal transaction for our deal, providing the desired end state (FR, FP or C)
+        $this->resetApi();
+        $this->api->setURL('/api/v1.5/deals/transactions/');
+        $this->api->setMethod('POST');
+        $this->api->addPayload('deal-id', $response['deals'][0]['id']);
+        $this->api->addPayload('status', 'FR'); // getting fees, which should be checked for sanity
+        $this->send();
+        $response = json_decode($this->api->getResult(), true);
+        $this->logApi();
+
+        if (empty($response['error'])) {
+            sleep(1); // just to be sure that we don't overwrite the first request/response pair
+            $this->resetApi();
+            $this->api->addPayload('deal-id', $response['deals'][0]['id']);
+            $this->api->addPayload('deal-status', 'C'); // accepting fees
+            $this->send();
+            $this->logApi();
+        }
+    }
 }
 
-if (empty($response['error'])) {
-	sleep(1); // just to be sure that we don't overwrite the first request/response pair
-	$retryAttempts = 0;
-	$api->resetPayload();
-	$api->addPayload('deal-id', $response['deals'][0]['id']);
-	$api->addPayload('deal-status', 'C'); // accepting fees
-	$api->send();
-	$response = json_decode($api->getResult(), true);
-	while ($retryAttempts++ < $retryMax && $response['error']['code'] == 'CADMV/Q023') {
-		error_log('DMV Retry Code Encountered');
-		sleep($retryDelayBase * pow(2, $retryAttempts));
-		$api->send();
-		$response = json_decode($api->getResult(), true);
-	}
-	Writer::writeRequestResponse($api);
-}
